0abb851812032ed058852b237dd423bd2ffd8126
Royal Finch <Royal2791@gmail.com>
2024-03-04 17:40:07 -0500

    Add inputs to PersonalDetail

--- src/components/EditPersonal.jsx
+++ src/components/EditPersonal.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+
 function EditPersonal() {
-  return <div className="edit-personal-container"></div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  return (
+    <div className="edit-personal-container">
+      <div className="input-container">
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="input-container">
+        <label>
+          Email
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="input-container">
+        <label>
+          Phone
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default EditPersonal;
